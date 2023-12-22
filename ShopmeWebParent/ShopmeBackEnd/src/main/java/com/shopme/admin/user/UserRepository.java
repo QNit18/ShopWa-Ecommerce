17a69039cf1760c393e609daf44f1b042c055da7
QNit18 <vanquang1833@gmail.com>
2023-12-22 16:44:30 +0700

    Export to CSV

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.shopme.admin.user;
 
 import com.shopme.common.entity.User;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,7 +19,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     
     public Long countById(Integer id);
-    
+
+    @Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ', u.lastName) LIKE %?1%")
+    public Page<User> findAll(String keyword, Pageable pageable);
+
     @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
     @Modifying
     public void updateEnabledStatus(Integer id, boolean enabled);
